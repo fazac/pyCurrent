ec0f4290b8dbabbfe3cbf9c47d12b5dd567fea8a
fazac <1650031931@qq.com>
2023-11-11 17:49:13 +0800

    first commit

---
+++ src/main/java/com/stock/pycurrent/PyCurrentApplication.java
@@ -0,0 +1,13 @@
+package com.stock.pycurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyCurrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyCurrentApplication.class, args);
+	}
+
+}
