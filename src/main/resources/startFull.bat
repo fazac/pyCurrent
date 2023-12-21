6d19656608b6f71cb8a549ab0cd33cc9cdba483f
Fazac <1650031931@qq.com>
2023-12-21 11:28:13 +0800

    文件整理

--- src/main/resources/startFull.bat
+++ src/main/resources/startFull.bat
@@ -2,7 +2,7 @@
 setlocal
 
 set JAVA_HOME=C:\Users\fa\Documents\programs\Payara\jdk
-set JAR_FILE=C:\Users\fa\.m2\repository\com\example\stockdemo\0.0.1-SNAPSHOT\stockdemo-0.0.1-SNAPSHOT.jar
+set JAR_FILE=C:\Users\fa\.m2\repository\com\example\stockdemo\0.0.1-SNAPSHOT\stockdemo-0.0.1-SNAPSHOT-exec.jar
 set LOG_FILE=C:\Users\fa\Desktop\log\full\spring.log
 
 echo Starting Java application...
