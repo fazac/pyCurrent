54ffc205a45adfb4d508c259f19fd7d9240d2829
Fazac <1650031931@qq.com>
2024-02-20 17:30:20 +0800

    bug修复

--- src/main/java/com/stock/pycurrent/entity/model/Constants.java
+++ src/main/java/com/stock/pycurrent/entity/model/Constants.java
@@ -7,9 +7,9 @@ public class Constants {
     public static String PERIOD_DAILY = "daily";
     public static String REHABILITATION_NO = "";
     public static String REHABILITATION_AFTER = "hfq";
-    public static String EM_DEFAULT_START_DAY = "19900101";
+    public static String EM_DEFAULT_START_DAY = "19860101";
     public static int EM_PULL_HOUR = 16;
-    public static String AKSHARE_EM_HIS = "akshare_his_stock.py";
+    public static String AKSHARE_EM_HIS = "akshare_his_stock_cur.py";
     public static String AKSHARE_EM_REALTIME = "akshare_stock_realtime.py";
     public static BigDecimal HUNDRED = BigDecimal.valueOf(100);
 
