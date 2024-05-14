9096589712413e84c0a82fefde82fc337b5d46e8
Fazac <1650031931@qq.com>
2024-05-14 09:54:37 +0800

    bug修复

--- src/main/java/com/stock/pycurrent/schedule/PullData.java
+++ src/main/java/com/stock/pycurrent/schedule/PullData.java
@@ -212,8 +212,15 @@ public class PullData implements CommandLineRunner {
             }
 
             String nameFirst = "N,C".contains(String.valueOf(tsName.charAt(0))) ? tsName.substring(1, 3) : tsName.substring(0, 2);
-            if (rt.getCurrentPri() != null && (tsCode.startsWith("0") || tsCode.startsWith("60")) && !tsName.contains("退") && !noConcerned && (limitCodeMap.containsKey(tsCode) && (limitCodeMap.get(tsCode).getCount() >= 2)) || holds || (concerned && !tsCode.startsWith("30"))) {
+            if (rt.getCurrentPri() != null
+                && (tsCode.startsWith("0") || tsCode.startsWith("60"))
+                && !tsName.contains("退")
+                && !noConcerned
+                && (limitCodeMap.containsKey(tsCode) && (limitCodeMap.get(tsCode).getCount() >= 2)
+                    || holds
+                    || concerned)) {
                 logsMap.get(holds ? "H" : concerned && !tsCode.startsWith("30") ? "C" : "A").add(fixPositiveLength(limitCodeMap.containsKey(tsCode) ? limitCodeMap.get(tsCode).getCount() : "", 2) + " " + (tsCode.charAt(0) + tsCode.substring(2, 6)) + fixLength(nameFirst, 3) + fixLength(rt.getPctChg(), 6) + fixLength(rt.getChangeHand(), 5) + fixLength("", 7) + fixLength("", 10) + fixLength("", 10) + fixLength(rt.getCurrentPri(), 6) + fixLength("", 11) + fixLength(rt.getVol() != null && checkOverLimit ? calBar(rt.getTsCode(), rt.getTradeDate(), rt.getCurrentPri()).multiply(THOUSAND).setScale(0, RoundingMode.FLOOR) : "", 8) + fixLength(rt.getCirculationMarketCap().divide(HUNDRED_MILLION, 3, RoundingMode.HALF_UP), 8) + fixLength(rt.getPe(), 8));
+                sendNewNotification(notification, rt, tsCode);
             }
 
             if (!tsName.contains("退") && tsCode.startsWith("3") && !noConcerned && rt.getPctChg() != null && checkOverLimit) {
@@ -236,7 +243,11 @@ public class PullData implements CommandLineRunner {
             }
             rangeOverLimit = valueCodeCountMap.containsKey(tsCode) && (convertTimeCount(nowClock) - valueCodeCountMap.get(tsCode) <= 40 || rt.getPctChg().compareTo(BigDecimal.TEN) >= 0);
             highLimit = rt.getPriHigh() != null && calRatio(rt.getPriHigh(), rt.getPriClosePre()).compareTo(PCH_LIMIT) > 0;
-            if (!noConcerned && !tsName.contains("退") && tsCode.startsWith("3") && rt.getPctChg() != null && (highLimit || concerned || holds || rangeOverLimit || yesterdayHigh)) {
+            if (!noConcerned
+                && !tsName.contains("退")
+                && tsCode.startsWith("3")
+                && rt.getPctChg() != null
+                && (highLimit || concerned || holds || rangeOverLimit || yesterdayHigh)) {
                 type = (concerned ? "C" : holds ? "H" : highLimit ? "F" : !yesterdayHigh ? "R" : "L");
                 if ((holds || concerned) && rt.getCurrentPri() != null && (stockMap.containsKey("HOLD_CODES") && stockMap.get("HOLD_CODES").containsKey(tsCode) || stockMap.containsKey("CONCERN_CODES") && stockMap.get("CONCERN_CODES").containsKey(tsCode))) {
                     EmConstantValue emConstantValue = stockMap.containsKey("HOLD_CODES") && stockMap.get("HOLD_CODES").containsKey(tsCode) ? stockMap.get("HOLD_CODES").get(tsCode) : stockMap.get("CONCERN_CODES").get(tsCode);
@@ -277,20 +288,8 @@ public class PullData implements CommandLineRunner {
                     volMap.put(tsCode, rt.getVol());
                     volStepMap.put(rt.getTsCode(), volStep);
                 }
-                if (rt.getPctChg() != null) {
-                    if (codeCountMap.containsKey(tsCode)) {
-                        if (CalculateUtils.reachTwentyLimit(rt)) {
-                            // max count++;
-                            codeCountMap.put(tsCode, codeCountMap.getOrDefault(tsCode, 0) + 1);
-                            if (codeCountMap.get(tsCode) < 2) {
-                                sendNotificationMsg("LIMIT ONE ", tsCode, notification);
-                            }
-                        }
-                    } else {
-                        codeCountMap.put(tsCode, 0);
-                        sendNotificationMsg("NEW ONE ", tsCode, notification);
-                    }
-
+                if (!rangeOverLimit) {
+                    sendNewNotification(notification, rt, tsCode);
                 }
             }
         }
@@ -313,6 +312,24 @@ public class PullData implements CommandLineRunner {
             curCountService.saveOne(curCount);
         }
 
+    }
+
+    private void sendNewNotification(boolean notification, EmRealTimeStock rt, String tsCode) {
+        if (rt.getPctChg() != null) {
+            if (codeCountMap.containsKey(tsCode)) {
+                if (CalculateUtils.reachTwentyLimit(rt)) {
+                    // max count++;
+                    codeCountMap.put(tsCode, codeCountMap.getOrDefault(tsCode, 0) + 1);
+                    if (codeCountMap.get(tsCode) < 2) {
+                        sendNotificationMsg("LIMIT ONE ", tsCode, notification);
+                    }
+                }
+            } else {
+                codeCountMap.put(tsCode, 0);
+                sendNotificationMsg("NEW ONE ", tsCode, notification);
+            }
+
+        }
     }
 
     private CurCount statisticsCurCount(List<EmRealTimeStock> stockList) {
