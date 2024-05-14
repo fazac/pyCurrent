8756162ea4a62d1b8d1361de075c6c90dbe17405
Fazac <1650031931@qq.com>
2024-05-14 11:29:20 +0800

    代码优化

--- src/main/java/com/stock/pycurrent/schedule/PullData.java
+++ src/main/java/com/stock/pycurrent/schedule/PullData.java
@@ -27,6 +27,8 @@ import java.util.stream.Collectors;
 @Configuration
 @CommonsLog
 public class PullData implements CommandLineRunner {
+
+    private static final int CHAR_LENGTH = 128;
     private static final BigDecimal HUNDRED = BigDecimal.valueOf(100);
     private static final BigDecimal THOUSAND = BigDecimal.valueOf(1000);
     private static final BigDecimal PCH_LIMIT = BigDecimal.valueOf(15);
@@ -205,7 +207,7 @@ public class PullData implements CommandLineRunner {
 
             String nameFirst = "N,C".contains(String.valueOf(tsName.charAt(0))) ? tsName.substring(1, 3) : tsName.substring(0, 2);
             if (rt.getCurrentPri() != null && (tsCode.startsWith("0") || tsCode.startsWith("60")) && !tsName.contains("退") && !noConcerned && (limitCodeMap.containsKey(tsCode) && (limitCodeMap.get(tsCode).getCount() >= 2)) || holds || (concerned && !tsCode.startsWith("30"))) {
-                logsMap.get(holds ? "H" : concerned && !tsCode.startsWith("30") ? "C" : "A").add(fixPositiveLength(limitCodeMap.containsKey(tsCode) ? limitCodeMap.get(tsCode).getCount() : "", 2) + " " + (tsCode.charAt(0) + tsCode.substring(2, 6)) + fixLength(nameFirst, 3) + fixLength(rt.getPctChg(), 6) + fixLength(rt.getChangeHand(), 5) + fixLength("", 7) + fixLength("", 10) + fixLength("", 10) + fixLength(rt.getCurrentPri(), 6) + fixLength("", 11) + fixLength(rt.getVol() != null && checkOverLimit ? calBar(rt.getTsCode(), rt.getTradeDate(), rt.getCurrentPri()).multiply(THOUSAND).setScale(0, RoundingMode.FLOOR) : "", 8) + fixLength(rt.getCirculationMarketCap().divide(HUNDRED_MILLION, 3, RoundingMode.HALF_UP), 8) + fixLength(rt.getPe(), 8));
+                logsMap.get(holds ? "H" : concerned && !tsCode.startsWith("30") ? "C" : "A").add(fixPositiveLength(limitCodeMap.containsKey(tsCode) ? limitCodeMap.get(tsCode).getCount() : "", 2) + " " + (tsCode.charAt(0) + tsCode.substring(2, 6)) + fixLength(nameFirst, 3) + fixLength(rt.getPctChg(), 6) + fixLength(rt.getChangeHand(), 5) + fixLength("", 7) + fixLength("", 7) + fixLength(rt.getCurrentPri(), 6) + fixLength("", 11) + fixLength(rt.getVol() != null && checkOverLimit ? calBar(rt.getTsCode(), rt.getTradeDate(), rt.getCurrentPri()).multiply(THOUSAND).setScale(0, RoundingMode.FLOOR) : "", 8) + fixLength(rt.getCirculationMarketCap().divide(HUNDRED_MILLION, 3, RoundingMode.HALF_UP), 8) + fixLength(rt.getPe(), 8));
             }
 
             if (!tsName.contains("退") && tsCode.startsWith("3") && !noConcerned && rt.getPctChg() != null && checkOverLimit) {
@@ -233,21 +235,11 @@ public class PullData implements CommandLineRunner {
                 if ((holds || concerned) && rt.getCurrentPri() != null && (stockMap.containsKey("HOLD_CODES") && stockMap.get("HOLD_CODES").containsKey(tsCode) || stockMap.containsKey("CONCERN_CODES") && stockMap.get("CONCERN_CODES").containsKey(tsCode))) {
                     EmConstantValue emConstantValue = stockMap.containsKey("HOLD_CODES") && stockMap.get("HOLD_CODES").containsKey(tsCode) ? stockMap.get("HOLD_CODES").get(tsCode) : stockMap.get("CONCERN_CODES").get(tsCode);
                     BigDecimal realRatio = calRatio(rt.getCurrentPri(), emConstantValue.getPrice());
-                    BigDecimal amount = rt.getCurrentPri().subtract(emConstantValue.getPrice()).multiply(BigDecimal.valueOf(emConstantValue.getVol()));
-                    BigDecimal potentialBenefits;
                     holdRemark.append(fixLength(realRatio, 7));
-                    if (emConstantValue.getProfit() != null) {
-                        potentialBenefits = amount.subtract(emConstantValue.getProfit());
-                        holdRemark.append(fixLength(emConstantValue.getProfit(), 10));
-                        holdRemark.append(fixLength(potentialBenefits.setScale(2, RoundingMode.HALF_UP), 10));
-                    } else {
-                        holdRemark.append(fixLength(amount, 10));
-                        holdRemark.append(fixLength("", 10));
-                    }
+                    holdRemark.append(fixLength(emConstantValue.getPrice(), 7));
                 } else {
                     holdRemark.append(fixLength("", 7));
-                    holdRemark.append(fixLength("", 10));
-                    holdRemark.append(fixLength("", 10));
+                    holdRemark.append(fixLength("", 7));
                 }
 
                 long volStep = 0L;
@@ -303,12 +295,12 @@ public class PullData implements CommandLineRunner {
                 }
             }
         }
-        log.warn(StockUtils.concatChar("_", 144));
-        String title = "|   TIME   |  I  |" + " T     CODE 简称 |  " + fixLengthTitle(" RT ", 4) + fixLengthTitle(" H ", 3) + fixLengthTitle(" RR  ", 5) + fixLengthTitle("   AM   ", 8) + fixLengthTitle("   PB   ", 8) + fixLengthTitle(" CP ", 4) + fixLengthTitle("VOL", 9) + fixLengthTitle("BAR", 6) + fixLengthTitle(" CM ", 6) + fixLengthTitle(" PE ", 6);
+        log.warn(StockUtils.concatChar("_", CHAR_LENGTH));
+        String title = "|   TIME   |  I  |" + " T     CODE 简称 |  " + fixLengthTitle(" RT ", 4) + fixLengthTitle(" H ", 3) + fixLengthTitle(" RR  ", 5) + fixLengthTitle(" BP ", 5) + fixLengthTitle(" CP ", 4) + fixLengthTitle("VOL", 9) + fixLengthTitle("BAR", 6) + fixLengthTitle(" CM ", 6) + fixLengthTitle(" PE ", 6);
         log.warn(title.substring(0, title.length() - 2));
-        log.warn(StockUtils.concatChar("‾", 144));
+        log.warn(StockUtils.concatChar("‾", CHAR_LENGTH));
         printMapInfo(logsMap, nowClock);
-        log.warn(StockUtils.concatChar("‾", 144));
+        log.warn(StockUtils.concatChar("‾", CHAR_LENGTH));
         if (!valueCodeCountMap.isEmpty() && refreshRangeOverCode) {
             List<RangeOverCodeValue> valueList = valueCodeCountMap.entrySet().stream().map(x -> new RangeOverCodeValue(x.getKey(), x.getValue())).toList();
             rangeOverCode.setCodeValue(valueList);
--- src/main/java/com/stock/pycurrent/util/StockUtils.java
+++ src/main/java/com/stock/pycurrent/util/StockUtils.java
@@ -1,23 +1,9 @@
 package com.stock.pycurrent.util;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.stock.pycurrent.entity.EmDAStock;
-import com.stock.pycurrent.entity.RocModel;
-import com.stock.pycurrent.entity.emum.EMSymbolEnum;
-import com.stock.pycurrent.entity.emum.PyFuncEnum;
-import com.stock.pycurrent.entity.model.Constants;
-import lombok.SneakyThrows;
 import lombok.extern.apachecommons.CommonsLog;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
 
 @CommonsLog
 public class StockUtils {
@@ -26,123 +12,13 @@ public class StockUtils {
         throw new IllegalStateException("StockUtils class");
     }
 
-    public static void initEmBasicData(String startDate, String endDate, String reType, String period, PyFuncEnum pyFuncEnum) {
-        ObjectNode objectNode = JSONUtils.getNode();
-        objectNode.put("start_date", DateUtils.getDateAtOffset(startDate, 1, ChronoUnit.DAYS));
-        objectNode.put("end_date", endDate);
-        objectNode.put("adjust", reType);
-        objectNode.put("period", period);
-        PythonScriptUtils.execThreadPY(Constants.AKSHARE_EM_HIS, pyFuncEnum + EMSymbolEnum.EM_STOCK.toString() + " --params=" + JSONUtils.toDoubleJson(objectNode));
-    }
-
-    public static String getPullHourEndDate() {
-        String endDate = DateUtils.now();
-        LocalDateTime now = LocalDateTime.now();
-        if (now.getDayOfWeek().equals(DayOfWeek.SATURDAY)
-            || now.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
-            endDate = DateUtils.getFriday(now);
-            return endDate;
-        } else {
-            boolean afterPullHour = now.getHour() >= Constants.EM_PULL_HOUR;
-            if (afterPullHour) {
-                return endDate;
-            } else {
-                return DateUtils.getDateAtOffset(endDate, -1, ChronoUnit.DAYS);
-            }
-        }
-    }
-
-    public static boolean afterPullHour() {
-        return LocalDateTime.now().getHour() == 15
-               && LocalDateTime.now().getMinute() >= 30
-               || LocalDateTime.now().getHour() > 15;
-    }
-
-    @SuppressWarnings("unused")
-    public static boolean inStartTimePeriod() {
-        return LocalDateTime.now().getHour() <= 15
-               && LocalDateTime.now().getMinute() <= 40;
-    }
-
     public static boolean isNotRest() {
         return LocalDateTime.now().getDayOfWeek() != DayOfWeek.SATURDAY
                && LocalDateTime.now().getDayOfWeek() != DayOfWeek.SUNDAY;
     }
 
-    public static boolean checkEmpty(String str) {
-        return str == null || str.isBlank();
-    }
-
     public static String concatChar(String value, int length) {
         return value.repeat(length);
-    }
-
-    public static boolean availableCode(String code) {
-        return !checkEmpty(code) && (code.startsWith("30") || code.startsWith("00") || code.startsWith("60"));
-    }
-
-    public static List<RocModel> findPeekPoint(List<EmDAStock> ts, String params, Date now) {
-        List<Integer> points = new ArrayList<>();
-        points.add(0);
-        points.add(ts.size() - 1);
-        return findCyclePeekPoint(ts, points, new ArrayList<>(), params, now);
-    }
-
-    @SneakyThrows
-    public static List<RocModel> findCyclePeekPoint(List<EmDAStock> ts, List<Integer> points, List<RocModel> res, String params, Date now) {
-        int oldSize = points.size();
-        List<Integer> pointCopy = new ArrayList<>(points);
-        for (int i = 0; i < pointCopy.size() - 1; i++) {
-            int startP = pointCopy.get(i);
-            List<EmDAStock> tmpTs = ts.subList(pointCopy.get(i), pointCopy.get(i + 1) + 1);
-            List<Integer> tmpPoints = findSectionPeekIndex(tmpTs).stream().filter(x -> x != 0 && x != tmpTs.size() - 1).toList();
-            if (!tmpPoints.isEmpty()) {
-                points.addAll(tmpPoints.stream().map(x -> x + startP).toList());
-                Collections.sort(points);
-            }
-        }
-        if (points.size() == oldSize) {
-            String code = ts.get(0).getTsCode();
-            for (int i = 0; i < points.size() - 1; i++) {
-                RocModel roc = new RocModel();
-                roc.setTsCode(code);
-                roc.setDoorPri(ts.get(points.get(i)).getPriClose());
-                roc.setCurClosePri(ts.get(points.get(i + 1)).getPriClose());
-                roc.setCount(points.get(i + 1) - points.get(i));
-                roc.setStartDate(ts.get(points.get(i)).getTradeDate());
-                roc.setEndDate(ts.get(points.get(i + 1)).getTradeDate());
-                roc.setParams(params);
-                roc.setCreateTime(now);
-                roc.setRatio(roc.getCurClosePri().subtract(roc.getDoorPri()).multiply(Constants.HUNDRED).divide(roc.getDoorPri(), 2, RoundingMode.HALF_UP));
-                res.add(roc);
-            }
-            return res;
-        } else {
-            return findCyclePeekPoint(ts, points, res, params, now);
-        }
-    }
-
-    @SneakyThrows
-    public static List<Integer> findSectionPeekIndex(List<EmDAStock> ts) {
-        BigDecimal doorPri;
-        BigDecimal min = BigDecimal.valueOf(9999);
-        BigDecimal max = BigDecimal.valueOf(-9999);
-        int minIndex = 0;
-        int maxIndex = 0;
-        BigDecimal curClosePri;
-        doorPri = ts.get(0).getPriClose();
-        for (int i = 1; i < ts.size(); i++) {
-            curClosePri = ts.get(i).getPriClose();
-            if (curClosePri.compareTo(min) < 0 && curClosePri.compareTo(doorPri) < 0) {
-                min = curClosePri;
-                minIndex = i;
-            }
-            if (curClosePri.compareTo(max) > 0 && curClosePri.compareTo(doorPri) > 0) {
-                max = curClosePri;
-                maxIndex = i;
-            }
-        }
-        return List.of(maxIndex, minIndex);
     }
 
 }
