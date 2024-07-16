35cd082d6e85e514d9347a1332ffb741b813c2b8
Fazac <1650031931@qq.com>
2024-07-16 15:57:57 +0800

    all 查询结果筛选

--- pyPages/src/views/RTAllCodeView.vue
+++ pyPages/src/views/RTAllCodeView.vue
@@ -1,14 +1,16 @@
 <script setup>
 import {onMounted} from 'vue'
 import ModelPage from '@/components/ModelPage.vue'
-import {cellStyle, headerCellStyle, tableData} from "@/api/util";
+import {isEmpty, tableData} from "@/api/util";
 import {findLast} from "@/api/backend";
 
 const rtData = tableData();
 
 function fetchRtAll() {
   findLast().then(res => {
-    rtData.value = res;
+    rtData.value = res.filter(item => {
+      return !isEmpty(item.currentPri);
+    });
   })
 }
 
